213c3f496db6e08c003ec4782e819b2628cf3b14
ryan kristoffer <ryanjarapa08@gmail.com>
2026-03-21 15:16:36 -0700

    Moved db configs to .env

--- scripts/config.py
+++ scripts/config.py
@@ -1,11 +1,15 @@
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DB_CONFIG = {
-    "host": "localhost",
-    "dbname": "steam_games_etl",
-    "user": "postgres",
-    "password": "0321-5674",
-    "port": 5432
+    "host": os.getenv("DB_HOST"),
+    "dbname": os.getenv("DB_NAME"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "port": os.getenv("DB_PORT")
 }
 
 PROJECT_ROOT_PATH = Path(__file__).resolve().parent.parent
